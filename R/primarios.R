8fcd996441657b6083fc2c56b95f44621fe0e1a6
José Enrique Pérez-Martín <jepm@uma.es>
2023-07-26 12:59:19 +0200

    mod

--- R/primarios.R
+++ R/primarios.R
@@ -5,100 +5,73 @@
 #' @return Tras correr este código se presentan los metabolitos que faltan por encontrar. Incluir los tiempos especificos en TagFinder para un segundo filtro
 #'
 
-primarios=function(set,tiempos=Tiempos_Especificos_Fruto){
-
-# DEFINIMOS LAS VARIABLES QUE VAN A SER UTILIZADAS
-
-  Matriz=NULL
-  MatrizFinal= NULL
-  Matriz2=NULL
-
-# HACEMOS UN FILTRO SELECCIONANDO SOLO AQUELLOS VALORES DOBLE TRUE
-
-  trues= set %>%
-    dplyr::filter(set[,1]=="true" & set[,2]=="true")
-
-  data=rbind(set[1:6,],trues)
-
-# SELECCIONAMOS AQUELLOS QUE COINCIDEN CON LA TABLA DE TIEMPOS APORTADA
-
-  for (i in 1:nrow(tiempos)){
-    for (n in 7:(nrow(data))){
-      if ((data[n,8]==tiempos[i,2]) & (data[n,16]==tiempos[i,3])){
-        Matriz=rbind(Matriz,data[n,])}
-    }}
-
-  Matriz=as.data.frame(Matriz)
-  Matriz[,8]=as.numeric(Matriz[,8])
-  Matriz[,15]=as.numeric(Matriz[,15])
-
-# PASAMOS A SELECCIONAR AQUELLAS FILAS QUE SON CORRECTAS
-
-  for (i in 1:nrow(tiempos))
-  {
-    for (a in 1:nrow(Matriz))
-    {
-      if (Matriz[a,16] == tiempos[i,3] & Matriz[a,8]==tiempos[i,2])
-      {
-        Matriz2= rbind(Matriz2, Matriz[a,]) #
-      }
-      else
-      {
-        v=c()
-        v=abs(Matriz2$V8 - Matriz2$V15)
-        MatrizFinal= rbind(MatrizFinal, Matriz2[which.min(v),])
-        Matriz2=NULL
-      }
-    }
-    if (!is.null(Matriz2))
-    {
-      v=c()
-      v=abs(Matriz2$V8 - Matriz2$V15)
-      MatrizFinal= rbind(MatrizFinal, Matriz2[which.min(v),])
-      Matriz2=NULL
+primarios_p <- function(set, tiempos = Tiempos_Especificos_Fruto) {
+  if (!(set %in% ls())) {
+    set <- read.delim(set, header = FALSE)
+  }
+  
+  # Seleccionamos solo aquellos valores doble true
+  trues <- set %>%
+    dplyr::filter(V1 == "true" & V2 == "true")
+  
+  data <- rbind(set[1:6, ], trues)
+  
+  # Seleccionamos aquellos que coinciden con la tabla de tiempos aportada
+  Matriz <- data %>%
+    dplyr::filter(V8 %in% tiempos[, 2] & V16 %in% tiempos[, 3])
+  
+  Matriz[, 8] <- as.numeric(Matriz[, 8])
+  Matriz[, 15] <- as.numeric(Matriz[, 15])
+  
+  # Seleccionamos aquellas filas que son correctas
+  MatrizFinal <- data.frame()
+  
+  for (i in 1:nrow(tiempos)) {
+    Matriz2 <- Matriz %>%
+      dplyr::filter(V16 == tiempos[i, 3] & V8 == tiempos[i, 2])
+    
+    if (nrow(Matriz2) > 0) {
+      v <- abs(Matriz2$V8 - Matriz2$V15)
+      MatrizFinal <- rbind(MatrizFinal, Matriz2[which.min(v), ])
     }
   }
-
-  MatrizFinal=rbind(data[1:6,],MatrizFinal)
-  MatrizFinal=as.data.frame(MatrizFinal)
-
-# SELECCIONAMOS AQUELLOS QUE FALTAN COMPARÁNDOLO CON LA MATRIZ TIEMPOS APORTADA
-
-  Faltan_Tiempos=c(setdiff(tiempos[,2],MatrizFinal[,8]))
-  Faltan=NULL
-
-  frases =c("DE PUTA MADRE", "ESTAN TODOS BRO", "INCREIBLE, PEDAZO DE SET", "TREMENDA MARAVILLA", "DE LOCOS HERMANO")
-
-# DEFINIMOS TODAS LAS POSIBLES OPCIONES PARA QUE EL CODIGO SEPA QUE HACER EN CADA CASO
-
-  if (any(dir()=="Resultados")==TRUE | any (dir("../")=="Resultados")){
-  } else {
-    dir.create("Resultados")
-  }
-
-if (!is.na(Faltan_Tiempos[1])){
-
-  for (i in 1:length(Faltan_Tiempos)){
-    for (n in 1:(nrow(tiempos))){
-      if ((tiempos[n,2]==Faltan_Tiempos[i])){
-        Faltan=rbind(Faltan,tiempos[n,])}
-    }
-  }
-    Faltan=Faltan[order(Faltan$Tag_Mass),]
-    resultados=(list(MatrizFinal,Faltan))
-
-    mostrar=function(x){
-      cat("\n","FALTAN LOS SIGUIENTES", "\n")
+  
+  MatrizFinal <- rbind(data[1:6, ], MatrizFinal)
+  
+  # Seleccionamos aquellos que faltan comparándolo con la matriz tiempos aportada
+  Faltan_Tiempos <- setdiff(tiempos[, 2], MatrizFinal[, 8])
+  
+  if (length(Faltan_Tiempos) > 0) {
+    Faltan <- tiempos %>%
+      dplyr::filter(tiempos[, 2] %in% Faltan_Tiempos) %>%
+      dplyr::arrange(Tag_Mass)
+    
+    resultados <- list(MatrizFinal, Faltan)
+    
+    mostrar <- function(x) {
+      cat("\n", "FALTAN LOS SIGUIENTES", "\n")
       print(x[[2]])
       invisible(x)
     }
-      mostrar(resultados)
+    
+    mostrar(resultados)
+    
+    assign("resultados", resultados, envir = .GlobalEnv)
+    
+    return(resultados)
+    
   } else {
-    if (any(dir("../")=="Resultados")){
-      xlsx::write.xlsx(MatrizFinal,"Results_Final.xlsx",col.names = FALSE, row.names = FALSE, showNA = TRUE)
-    } else if (any(dir)=="Resultados"){
-      xlsx::write.xlsx(MatrizFinal,"Resultados/Results_Final.xlsx",col.names = FALSE, row.names = FALSE, showNA = TRUE)
+    if (any(dir("../") == "Resultados")) {
+      xlsx::write.xlsx(MatrizFinal, "Results_Final.xlsx", col.names = FALSE, row.names = FALSE, showNA = TRUE)
+    } else if (any(dir() == "Resultados")) {
+      xlsx::write.xlsx(MatrizFinal, "Resultados/Results_Final.xlsx", col.names = FALSE, row.names = FALSE, showNA = TRUE)
+    } else {
+      dir.create("Resultados")
+      xlsx::write.xlsx(MatrizFinal, "Resultados/Results_Final.xlsx", col.names = FALSE, row.names = FALSE, showNA = TRUE)
     }
+    
+    assign("resultados", MatrizFinal, envir = .GlobalEnv)
+    
+    return(MatrizFinal)
   }
-
 }
--- R/primarios_faltan.R
+++ R/primarios_faltan.R
@@ -6,119 +6,104 @@
 #' @return Tras correr este código se presentan los metabolitos que faltan por encontrar.
 #'
 
-primarios_faltan=function(set2,set_primarios,tiempos=Tiempos_Especificos_Fruto){
-
-  MatrizFinal=set_primarios[[1]]
-  Faltan=set_primarios[[2]]
-
-#  require(magrittr)
-
-  trues2=set2 %>%
-    dplyr::filter(set2[,1]=="true" & set2[,2]=="true")
-
-  faltan=rbind(set2[1:6,],trues2)
-
-  Matriz_faltan=NULL
-
-  for (i in 1:nrow(Faltan)){
-    for (n in 7:(nrow(faltan))){
-      if ((faltan[n,8]==Faltan[i,2]) & (faltan[n,16]==Faltan[i,3])){
-        Matriz_faltan=rbind(Matriz_faltan,faltan[n,])}
-    }}
-
-if (!is.null(Matriz_faltan)){
-  Matriz_faltan=as.data.frame(Matriz_faltan)
-  Matriz_faltan[,8]=as.numeric(Matriz_faltan[,8])
-  Matriz_faltan[,15]=as.numeric(Matriz_faltan[,15])
-
-}
-
-  ##########################################
-
-  nombres=MatrizFinal[1:6,]
-  Matriz_faltan=rbind(MatrizFinal[-(1:6),],Matriz_faltan)
-  Matriz_faltan[,8]=as.numeric(Matriz_faltan[,8])
-  Matriz_faltan[,15]=as.numeric(Matriz_faltan[,15])
-
-  Matriz2_faltan=NULL
-
-  Matriz_faltan=dplyr::arrange(Matriz_faltan,Matriz_faltan[,8])
-
-  MatrizFinal_faltan=NULL
-
-
-  for (i in 1:nrow(tiempos))
-  {
-    for (a in 1:nrow(Matriz_faltan))
-    {
-      if (Matriz_faltan[a,16] == tiempos[i,3] & Matriz_faltan[a,8]==tiempos[i,2])
-      {
-        Matriz2_faltan= rbind(Matriz2_faltan, Matriz_faltan[a,]) #
+primarios_faltan_p <- function(set2, set_primarios=resultados, tiempos = Tiempos_Especificos_Fruto) {
+  
+  if (!(set2 %in% ls())) {
+    set2 <- read.delim(set2, header = FALSE)
+  }
+  
+  MatrizFinal <- set_primarios[[1]]
+  Faltan <- set_primarios[[2]]
+  
+  trues2 <- set2 %>%
+    dplyr::filter(V1 == "true" & V2 == "true")
+  
+  faltan <- rbind(set2[1:6, ], trues2)
+  
+  Matriz_faltan <- NULL
+  
+  for (i in 1:nrow(Faltan)) {
+    for (n in 7:(nrow(faltan))) {
+      if ((faltan[n, 8] == Faltan[i, 2]) & (faltan[n, 16] == Faltan[i, 3])) {
+        Matriz_faltan <- rbind(Matriz_faltan, faltan[n, ])
       }
-      else #if (Matriz[a, 8] != tiempos[i,1])
-      {
-        p=abs(Matriz2_faltan$V8 - Matriz2_faltan$V15)
-        MatrizFinal_faltan= rbind(MatrizFinal_faltan, Matriz2_faltan[which.min(p),])
-        Matriz2_faltan=NULL
-      }
-    }
-    if (!is.null(Matriz2_faltan))
-    {
-      v=c()
-      v=abs(Matriz2_faltan$V8 - Matriz2_faltan$V15)
-      MatrizFinal_faltan= rbind(MatrizFinal_faltan, Matriz2_faltan[which.min(v),])
-      Matriz2_faltan=NULL
     }
   }
-
-  MatrizFinal=as.data.frame(MatrizFinal)
-
-  ######################################
-
-  frases =c("DE PUTA MADRE", "ESTAN TODOS BRO", "INCREIBLE, PEDAZO DE SET", "TREMENDA MARAVILLA", "DE LOCOS HERMANO")
-
-  Faltan_Tiempos=c(setdiff(tiempos[,2],MatrizFinal_faltan[,8]))
-  Faltan=NULL
-
-if (!is.na(Faltan_Tiempos[1])){
-  for (i in 1:length(Faltan_Tiempos)){
-    for (n in 1:(nrow(tiempos))){
-      if ((tiempos[n,2]==Faltan_Tiempos[i])){
-        Faltan=rbind(Faltan,tiempos[n,])}
-    }}}
-
-  if (any(dir()=="Resultados")==TRUE | any (dir("../")=="Resultados")){
-  } else {
+  
+  if (!is.null(Matriz_faltan)) {
+    Matriz_faltan <- as.data.frame(Matriz_faltan)
+    Matriz_faltan[, 8] <- as.numeric(Matriz_faltan[, 8])
+    Matriz_faltan[, 15] <- as.numeric(Matriz_faltan[, 15])
+  }
+  
+  nombres <- MatrizFinal[1:6, ]
+  Matriz_faltan <- rbind(MatrizFinal[-(1:6), ], Matriz_faltan)
+  Matriz_faltan[, 8] <- as.numeric(Matriz_faltan[, 8])
+  Matriz_faltan[, 15] <- as.numeric(Matriz_faltan[, 15])
+  
+  Matriz2_faltan <- NULL
+  Matriz_faltan <- dplyr::arrange(Matriz_faltan, V8)
+  MatrizFinal_faltan <- NULL
+  
+  for (i in 1:nrow(tiempos)) {
+    for (a in 1:nrow(Matriz_faltan)) {
+      if (Matriz_faltan[a, 16] == tiempos[i, 3] & Matriz_faltan[a, 8] == tiempos[i, 2]) {
+        Matriz2_faltan <- rbind(Matriz2_faltan, Matriz_faltan[a, ])
+      } else {
+        p <- abs(Matriz2_faltan$V8 - Matriz2_faltan$V15)
+        MatrizFinal_faltan <- rbind(MatrizFinal_faltan, Matriz2_faltan[which.min(p), ])
+        Matriz2_faltan <- NULL
+      }
+    }
+    if (!is.null(Matriz2_faltan)) {
+      v <- abs(Matriz2_faltan$V8 - Matriz2_faltan$V15)
+      MatrizFinal_faltan <- rbind(MatrizFinal_faltan, Matriz2_faltan[which.min(v), ])
+      Matriz2_faltan <- NULL
+    }
+  }
+  
+  MatrizFinal <- as.data.frame(MatrizFinal)
+  
+  frases <- c("DE PUTA MADRE", "ESTAN TODOS BRO", "INCREIBLE, PEDAZO DE SET", "TREMENDA MARAVILLA", "DE LOCOS HERMANO")
+  
+  Faltan_Tiempos <- setdiff(tiempos[, 2], MatrizFinal_faltan[, 8])
+  Faltan <- NULL
+  
+  if (!is.na(Faltan_Tiempos[1])) {
+    for (i in 1:length(Faltan_Tiempos)) {
+      for (n in 1:(nrow(tiempos))) {
+        if ((tiempos[n, 2] == Faltan_Tiempos[i])) {
+          Faltan <- rbind(Faltan, tiempos[n, ])
+        }
+      }
+    }
+  }
+  
+  if (!dir.exists("Resultados") && !dir.exists("../Resultados")) {
     dir.create("Resultados")
   }
-
-if(!is.null(Faltan)){
-  Faltan_relleno=matrix(NA,nrow(Faltan),ncol(MatrizFinal_faltan)-14)
-  Faltan_relleno=cbind(Faltan,Faltan_relleno)
-# ESTO LO DEJO POR SI LO INCLUYO EN EL FUTURO  Faltan_relleno=cbind(matrix(data=NA,nrow(Faltan),3),Faltan [,1],matrix(data=NA,nrow(Faltan),3),Faltan[,2],matrix(data=NA,nrow(Faltan),5),Faltan[,3],Faltan_relleno)
-  Faltan_relleno=cbind(matrix(data=NA,nrow(Faltan_relleno),3),Faltan_relleno)
-
-  for (j in 1:nrow(Faltan_relleno)){
-    MatrizFinal_faltan=berryFunctions::insertRows(MatrizFinal_faltan,(as.numeric(rownames(Faltan_relleno[j,]))+(1-j)),Faltan_relleno[j,])
+  
+  if (!is.null(Faltan)) {
+    Faltan_relleno <- matrix(NA, nrow(Faltan), ncol(MatrizFinal_faltan) - 14)
+    Faltan_relleno <- cbind(Faltan, Faltan_relleno)
+    Faltan_relleno <- cbind(matrix(data = NA, nrow(Faltan_relleno), 3), Faltan_relleno)
+    
+    for (j in 1:nrow(Faltan_relleno)) {
+      MatrizFinal_faltan <- berryFunctions::insertRows(MatrizFinal_faltan, (as.numeric(rownames(Faltan_relleno[j, ])) + (1 - j)), Faltan_relleno[j, ])
+    }
+  }
+  
+  MatrizFinal_faltan <- rbind(nombres, MatrizFinal_faltan)
+  
+  output_path <- ifelse(any(dir("../") == "Resultados"), "../Resultados", "Resultados")
+  
+  xlsx::write.xlsx(MatrizFinal_faltan, file.path(output_path, "Results_Final.xlsx"), col.names = FALSE, row.names = FALSE, showNA = TRUE)
+  
+  if (!is.null(Faltan)) {
+    Faltan <- Faltan[order(Faltan$Tag_Mass), ]
+    xlsx::write.xlsx(Faltan, file.path(output_path, "Faltan.xlsx"), col.names = FALSE, row.names = FALSE, showNA = FALSE)
+    return(Faltan)
+  } else {
+    cat("\n", sample(frases, 1), "\n", homer)
   }
 }
-
-  MatrizFinal_faltan=rbind(nombres,MatrizFinal_faltan)
-
-  if(any(dir("../")=="Resultados")){
-    xlsx::write.xlsx(MatrizFinal_faltan,"Results_Final.xlsx",col.names = FALSE, row.names = FALSE, showNA = TRUE)}
-  else if (any(dir()=="Resultados")){
-    xlsx::write.xlsx(MatrizFinal_faltan,"Resultados/Results_Final.xlsx",col.names = FALSE, row.names = FALSE, showNA = TRUE)}
-
-if (!is.null(Faltan)){
-    Faltan=Faltan[order(Faltan$Tag_Mass),]
-    if(any(dir("../")=="Resultados")){
-    xlsx::write.xlsx(Faltan,file="Faltan.xlsx",col.names = FALSE, row.names = FALSE, showNA = FALSE)}
-    else if (any(dir()=="Resultados")){
-      xlsx::write.xlsx(Faltan,file="Resultados/Faltan.xlsx",col.names = FALSE, row.names = FALSE, showNA = FALSE)}
-    return(Faltan)
-} else {
-  cat("\n",sample(frases,1),"\n", homer)
-}
-}
-
